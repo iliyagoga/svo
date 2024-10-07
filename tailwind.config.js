41f25960d3b64677e81b078b93c1bbdd7487b874
iliyagoga <100951861+iliyagoga@users.noreply.github.com>
2024-10-07 23:19:32 +0400

    07.10.2024
    theme2 update

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,7 @@ module.exports = {
       },
       colors:{
         'bsss':'#81CBE7',
+        'chor':'#BCB679'
        
         
       },
@@ -17,7 +18,8 @@ module.exports = {
         ...theme('colors'),
         'bg-black': '#1D1D1D',
         'base':'#81CBE7',
-        'h_bg':'#1f97c5d9'
+        'h_bg':'#1f97c5d9',
+        'chor':'#BCB679'
        
         
       }),
@@ -36,8 +38,13 @@ module.exports = {
       backgroundImage:{
          'bg-fsp':'radial-gradient(50% 50% at 50% 50%, #81CBE7 0%, rgba(34, 34, 34, 0) 100%)',
          'bg_sh':'linear-gradient(180deg, rgba(29, 29, 29, 0) 0%, #1D1D1D 69.97%)',
+         'bg-mean2-mg':"url('../assets/imgs/Mask group.png')",
          'bg-mean2-mg2':"url('../assets/imgs/Mask group (2).png')",
-         'bg-mean2-mg3':"url('../assets/imgs/Mask group (3).png')"
+
+         'bg-mean2-mg3':"url('../assets/imgs/Mask group (3).png')",
+         
+         'gradient-theme2-yellow':'linear-gradient(90deg, #DDD8AF 0%, #FFFFFF 100%)',
+         'necro-gradient-theme2-yellow':'linear-gradient(90deg, #FFFFFF 0%, #DDD8AF 100%)'
       }
     },
   },
