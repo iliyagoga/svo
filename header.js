c5d69b99b58f9fd518a26aa635aa19f9ea3e8716
iliyagoga <100951861+iliyagoga@users.noreply.github.com>
2024-11-12 16:47:36 +0400

    12.11.2024 3

--- header.js
+++ header.js
@@ -2,7 +2,17 @@ let icon= document.querySelector('#icon')
 icon.addEventListener('click',(e)=>{
     document.querySelector('#menu').classList.toggle('hidden')
 })
+document.addEventListener('click',(e)=>{
+    if(e.target!=p1 && e.target!=p2  && e.target!=p3 ){
+        if(!f1.classList.contains('invisible'))
+        f1.classList.add('invisible')
+        if(!f2.classList.contains('invisible'))
+        f2.classList.add('invisible')
+        if(!f3.classList.contains('invisible'))
+        f3.classList.add('invisible')
+    }
 
+})
 document.querySelector('#menu').addEventListener('click',(e)=>{
     if(e.target.nodeName=='A'){
         document.querySelector('#menu').classList.toggle('hidden')
@@ -46,59 +56,4 @@ p3.addEventListener('click',(e)=>{
     }
 })
 
-let bp1=document.querySelector('#bp1');
-let bf1=document.querySelector('#bf1');
-bp1.addEventListener('click',(e)=>{
-    bf1.classList.toggle('hidden')
-    if(!bf2.classList.contains('hidden')){
-        bf2.classList.add('hidden')
-    }
-    if(!bf3.classList.contains('hidden')){
-        bf3.classList.add('hidden')
-    }
- 
-})
 
-let bp2=document.querySelector('#bp2');
-let bf2=document.querySelector('#bf2');
-bp2.addEventListener('click',(e)=>{
-    bf2.classList.toggle('hidden')
-    if(!bf1.classList.contains('hidden')){
-        bf1.classList.add('hidden')
-    }
-    if(!bf3.classList.contains('hidden')){
-        bf3.classList.add('hidden')
-    }
-})
-
-let bp3=document.querySelector('#bp3');
-let bf3=document.querySelector('#bf3');
-bp3.addEventListener('click',(e)=>{
-    bf3.classList.toggle('hidden')
-    if(!bf1.classList.contains('hidden')){
-        bf1.classList.add('hidden')
-    }
-    if(!bf2.classList.contains('hidden')){
-        bf2.classList.add('hidden')
-    }
-})
-
-let b_ic =document.querySelector('#b_ic')
-let b_menu =document.querySelector('#b_menu')
-let b=false
-b_ic.addEventListener('click',(e)=>{
-    if(b===false){
-        b_menu.classList.remove('-left-full')
-        b_menu.classList.add('left-0')
-        b=true
-    }
-    else{
-        b_menu.classList.remove('left-0')
-        b_menu.classList.add('-left-full')
-        bf1.classList.add('hidden')
-        bf2.classList.add('hidden')
-        bf3.classList.add('hidden')
-
-        b=false
-    }
-})
